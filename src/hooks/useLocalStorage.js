c123df0add2ad4aecd551a1b3bff82258012f898
Zac Higgins <zachiggins@Higgins-iMac.local>
2019-10-31 08:18:00 -0600

    copied code from another student to help debug

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,9 +1,11 @@
 import { useState } from 'react';
-
 export const useLocalStorage = (key, initialValue) => {
     const [storedValue, setStoredValue] = useState(() => {
-        const item = window.localStorage.getItem(key);
-        return item ? JSON.parse(item) : initialValue;
+        if (window.localStorage.getItem(key)) {
+            return JSON.parse(window.localStorage.getItem(key));
+        }
+        window.localStorage.setItem(key, JSON.stringify(initialValue));
+        return initialValue;
     });
 
     const setValue = value => {
@@ -11,5 +13,5 @@ export const useLocalStorage = (key, initialValue) => {
         window.localStorage.setItem(key, JSON.stringify(value));
     };
 
-    return [storedValue, setValue];
-};
+    return [storedValue, setValue]
+}
